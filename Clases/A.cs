2073e21ceab4784a39319cf2eb423a930481cf13
Ivan Biscaldi <ivanbisc12@gmail.com>
2021-05-25 02:01:30 -0300

    Lab 01

---
+++ Clases/A.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Clases
+{
+    public class A
+    {
+        private String _nombreInstancia;
+
+        public String NombreInstancia
+        {
+            get
+            {
+                return _nombreInstancia;
+            }
+            set
+            {
+                _nombreInstancia = value;
+            }
+        }
+
+        public A()
+        {
+            this.NombreInstancia = "Instancia sin nombre";
+        }
+
+        public A(String nombre)
+        {
+            this.NombreInstancia = nombre;
+        }
+
+        public void MostrarNombre()
+        {
+            Console.WriteLine(this.NombreInstancia);
+        }
+
+        public void M1()
+        {
+            Console.WriteLine("\nInvocaste el metodo M1\n");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("\nInvocaste el metodo M2\n");
+        }
+
+        public void M3()
+        {
+            Console.WriteLine("\nInvocaste el metodo M3\n");
+        }
+    }
+}
---
+++ Clases/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class B : A
+    {
+        /*public B(a) : base(a) 
+        {
+            //
+        }
+        */
+        public void M4()
+        {
+            Console.WriteLine("\nMetodo del hijo invocado\n");
+        }
+    }
+}
