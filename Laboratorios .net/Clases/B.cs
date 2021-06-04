73ddf66ac11fa794fca8c12950adf35956de9213
Ivan Biscaldi <ivanbisc12@gmail.com>
2021-06-03 21:09:25 -0300

    Correccion LabClases

---
+++ Laboratorios .net/Clases/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class B : A
+    {
+        public B() : base("Metodo de la clase B") 
+        {
+            //
+        }
+        
+        public void M4()
+        {
+            Console.WriteLine("\nMetodo del hijo invocado\n");
+        }
+    }
+}
